fa486a0ac694e10a0a75f7aa0b29354c54eae84b
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-10-14 23:06:00 -0400

    Update Workout.kt

--- app/src/main/java/com/example/regiment/data/Workout.kt
+++ app/src/main/java/com/example/regiment/data/Workout.kt
@@ -1,14 +1,6 @@
 package com.example.regiment.data
-import java.time.localDate // not sure yet
 
-enum class WorkoutType {
-    WEIGHT_TRAINING,
-    CARDIO,
-    CALISTHENICS
-}
-
-    val id: String = java.util.UUID.randomUUID().toString(), // name of workout
-    val date: LocalDate, // When the workout is/was done
-    val type: WorkoutType, // workout category
-    val exercises: List<Exercise> = emptyList(), // List of exercises
-    val isPlanned: Boolean = false // planned head of a workout
+    val id: Int,
+    val date: String, // When the workout is/was done
+    val type: String, // workout category
+    
