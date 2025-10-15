095f878cc0110af0ee62227e562f5605ad11e7d7
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-10-14 23:04:00 -0400

    Update Exercise.kt

--- app/src/main/java/com/example/regiment/data/Exercise.kt
+++ app/src/main/java/com/example/regiment/data/Exercise.kt
@@ -1,1 +1,10 @@
+/*package com.example.regiment.data
+
+    val name: String, // naming the actual
+    val sets: Int? = null, // usual counts at the gym 
+    val reps: Int? = null, //  usual counts at the gym 
+    val weight: Double? = null,     // For weights
+    val durationMinutes: Int? = null, // For time-based excersie
+    val notes: String? = null // user notes about the excercise 
+    */
 
