90ef55fd39e21432dbdace982619646faa20605e
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-10-19 07:22:57 -0500

    Update Color.kt

--- app/src/main/java/com/example/regiment/ui/theme/Color.kt
+++ app/src/main/java/com/example/regiment/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.regiment.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBackground = Color(0xFFFFFFFF) // white
+val LightPrimary = Color(0xFFFF4D4D)     // red
+val LightSecondary = Color(0xFFFFC1E3)   // light pink
+val LightText = Color(0xFF000000)        // black
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBackground = Color(0xFF121212)   // typical dark mode background
+val DarkPrimary = Color(0xFFFF6B6B)       // softer red
+val DarkSecondary = Color(0xFFFFA6C9)     // softer pink
+val DarkText = Color(0xFFFFFFFF)          // white
