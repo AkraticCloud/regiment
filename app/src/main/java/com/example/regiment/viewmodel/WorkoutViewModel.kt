175d8adbba43bc78a6f78ccba7d728e1cb06bf51
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-10-19 07:47:19 -0500

    Update WorkoutViewModel.kt
    added a filter button for the list

--- app/src/main/java/com/example/regiment/viewmodel/WorkoutViewModel.kt
+++ app/src/main/java/com/example/regiment/viewmodel/WorkoutViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.regiment.data
 
 class WorkoutViewModel : ViewModel() {
-    private val workouts = mutableStateListOf<Workout>()
+    private val workoutss = mutableStateListOf<Workout>()
     //behave like MutableList<Workout> notifies when their is a list change
     val workouts: List<Workout>
     // allows you to access with out editing the data encapsulation
@@ -11,11 +11,16 @@ class WorkoutViewModel : ViewModel() {
 
    //function adds a new workout to the list, assigns a unique ID and then increments the counter.
   fun addWorkout(type: String, date: String) {
-        workouts.add(Workout(id = currentId++, type = type, date = date))
+        workoutss.add(Workout(id = currentId++, type = type, date = date))
     }
 
     //function deletes a workout by ID, 
     //removeIf is a Kotlin/Java function that removes all items matching the condition
     fun deleteWorkout(id: Int) {
-        _workouts.removeIf { it.id == id }
+        workoutss.removeIf { it.id == id }
     }
+
+    fun filterByType(type: String): List<Workout> {
+        return workoutss.filter { it.type.equals(type, ignoreCase = true) }
+    }
+}
