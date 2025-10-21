9a683eabbb784be11d870e08032b41d6bb578186
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-10-21 01:07:24 -0400

    Update Workout.kt

--- app/src/main/java/com/example/regiment/data/Workout.kt
+++ app/src/main/java/com/example/regiment/data/Workout.kt
@@ -1,6 +1,7 @@
 package com.example.regiment.data
-
+data class Workout(
     val id: Int,
     val date: String, // When the workout is/was done
     val type: String, // workout category
-    
+    val description: String
+)
