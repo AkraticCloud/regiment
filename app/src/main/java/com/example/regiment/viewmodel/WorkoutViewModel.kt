81b61482f5d7beb4986a353d201e39d37e85db55
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-10-21 01:03:04 -0400

    Update WorkoutViewModel.kt

--- app/src/main/java/com/example/regiment/viewmodel/WorkoutViewModel.kt
+++ app/src/main/java/com/example/regiment/viewmodel/WorkoutViewModel.kt
@@ -1,26 +1,32 @@
-package com.example.regiment.data
+package com.example.regiment.viewmodel
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import com.example.regiment.data.Workout
 
 class WorkoutViewModel : ViewModel() {
     private val workoutss = mutableStateListOf<Workout>()
     //behave like MutableList<Workout> notifies when their is a list change
-    val workouts: List<Workout> get() = Workouts
+    val workouts: List<Workout> get() = workoutss
     // allows you to access with out editing the data encapsulation
 
-     private var currentId = 0
+
     //simple counter to assign a unique ID to each Workout added
 
    //function adds a new workout to the list, assigns a unique ID and then increments the counter.
-  fun addWorkout(type: String, date: String) {
-        workoutss.add(Workout(id = currentId++, type = type, date = date))
+  fun addWorkout(type: String, date: String, description: String) {
+       val newWorkout = Workout(
+           id = ( workoutss.maxOfOrNull { it.id } ?: 0) + 1,
+           type = type,
+           date = date,
+           description = description
+       )
+       workoutss.add(newWorkout)
     }
 
     //function deletes a workout by ID, 
     //removeIf is a Kotlin/Java function that removes all items matching the condition
     fun deleteWorkout(id: Int) {
         workoutss.removeIf { it.id == id }
-    }
-
-    fun filterByType(type: String): List<Workout> {
-        return workoutss.filter { it.type.equals(type, ignoreCase = true) }
     }
 }
