80c562050b64f3813ee1fd2f81ba168efc4078e8
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-11-17 18:02:45 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/regiment/MainActivity.kt
+++ app/src/main/java/com/example/regiment/MainActivity.kt
@@ -1,3 +1,12 @@
+package com.example.regiment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
