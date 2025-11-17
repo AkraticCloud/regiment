a51bd79c1fa12632c870aa3c72c3b5f409ef420a
SommieOgu <144720270+SommieOgu@users.noreply.github.com>
2025-11-17 18:02:26 -0500

    Update MainActivity.kt
    fixed viewModel moved dumby list to a different page

--- app/src/main/java/com/example/regiment/MainActivity.kt
+++ app/src/main/java/com/example/regiment/MainActivity.kt
@@ -1,12 +1,3 @@
-package com.example.regiment
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -43,31 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainActivityPreview() {
     RegimentTheme {
-
-        val dummyViewModel = WorkoutViewModel().apply {
-            addWorkout(
-                category = WorkoutCategory.CARDIO,
-                subType = "Running",
-                date = "2025-10-19",
-                description = "Ran a 5k"
-            )
-
-            addWorkout(
-                category = WorkoutCategory.WEIGHT_TRAINING,
-                subType = "Bench Press",
-                date = "2025-10-20",
-                description = "Bench Press 3x10"
-            )
-
-            addWorkout(
-                category = WorkoutCategory.CALISTHENICS,
-                subType = "Planks",
-                date = "2025-10-21",
-                description = "Core workout"
-            )
-        }
-
-        WorkoutScreen(viewModel = dummyViewModel)
+        WorkoutScreen(viewModel = WorkoutViewModel())
 
     }
 }
